b271b86beab83662506698a5221682c5523264d2
Bourkupine <euanbourke@gmail.com>
2022-04-07 21:28:13 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    private final int[][] sudoku = {
+    {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9},
+    {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9},
+    {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9},
+    {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9},
+    {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9},
+    {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9},
+    {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9},
+    {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9},
+    {1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9}};
+
+
+
+    public static void main(String[] args){
+
+
+
+    }
+
+    public boolean RowChecker(String[] board) {
+
+        return true;
+    }
+    public boolean ColChecker(String[] board) {
+
+        return true;
+    }
+    public boolean BoxChecker(String[] board) {
+
+        return true;
+    }
+
+
+}
